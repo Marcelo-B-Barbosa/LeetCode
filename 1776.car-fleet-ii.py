d3060cc3de14b68f9211b7cdaf805e01f0f8b2c6
Marcelo Barbosa <marcelo.b.barbosa@gmail.com>
2023-06-12 12:00:21 +0800

    Add some edited files

--- 1776.car-fleet-ii.py
+++ 1776.car-fleet-ii.py
@@ -1,15 +1,16 @@
-from typing import List
+#
+# @lc app=leetcode id=1776 lang=python3
+#
+# [1776] Car Fleet II
+#
 
+# @lc code=start
 class Solution:
     def getCollisionTimes(self, cars: List[List[int]]) -> List[float]:
-        cars.sort(reverse=True) # Sort by the closest to the finish line
-        carsStack = []
-        collisions = []
 
-        for p, s in cars:
-            
+        pass
 
-
+    # Solution from Problem 853.car-fleet
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair = [(p, s) for p, s in zip(position, speed)]
         pair.sort(reverse=True)
@@ -19,10 +20,6 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-    
+        
+# @lc code=end
 
-s = Solution()
-
-cars = [[1,2],[2,1],[4,3],[7,2]]
-
-print(s.getCollisionTimes(cars))
